0fe05cb411b6505836661a6f63e275a52f3ad98c
Aizquel Urdaneta <aizquelus@gmail.com>
2023-11-17 19:13:11 -0300

    Optimizando componente footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,19 @@
-import { Box, Flex, Image, Text, Spacer, HStack, Button } from '@chakra-ui/react'
+import { Flex, Image, Text, HStack, Button } from '@chakra-ui/react'
 
 const Footer = () => {
     return (
         <footer>
-            <Flex px="5%" py="3%" minH="15vh" justify="flex-end" direction="column">
-                <Flex justify="space-between" pb="15px">
-                    <Image className="logo" src="/img/gamebit-logo-text.png" alt="GameBit Logo" maxW="150px" />
-                    <Spacer />
-                    <HStack>
-                        <Button colorScheme='facebook' >
-                            Facebook
-                        </Button>
-                        <Button colorScheme='twitter'>
-                            Twitter
-                        </Button>
-                    </HStack>
-                </Flex>
-
-                <Box>
-                    <Text fontSize="lg" textAlign="center">© 2023 Gamebit. All rights reserved.</Text>
-                </Box>
+            <Flex px="5%" py="3%" minH="15vh" className='ftr-fst-child'>
+                <Image className="logo" src="/img/gamebit-logo-text.png" alt="GameBit Logo" maxW="150px" display="inline-block" />
+                <Text fontSize="lg" textAlign="center">© 2023 Gamebit. All rights reserved.</Text>
+                <HStack>
+                    <Button colorScheme='facebook' >
+                        Facebook
+                    </Button>
+                    <Button colorScheme='twitter'>
+                        Twitter
+                    </Button>
+                </HStack>
             </Flex>
         </footer>
     )
