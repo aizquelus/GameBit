8116ebd42e82b679443789363740b72100afcc4a
Aizquel Urdaneta <aizquelus@gmail.com>
2023-11-17 13:45:03 -0300

    Reorganizando layout del carrito en mobile

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -11,9 +11,9 @@ const Cart = () => {
   return (
     <Center minH='70vh' flexDirection="column" p="5%" justifyContent={cart.length > 0 ? "flex-start" : "center"} alignItems={cart.length > 0 ? "flex-start" : "center"}>
       
-          {cart.length > 0 && <Flex display="flex" alignItems="center" w="100%" ><Checkout /></Flex>}
+          {cart.length > 0 && <Flex display="flex" alignItems="center" w="100%" className="container"><Checkout /></Flex>}
       
-      <Flex alignItems={cart.length > 0 ? "flex-end" : "center"} flexDirection={cart.length > 0 ? "row" : "column"}>
+      <Flex alignItems={cart.length > 0 ? "flex-end" : "center"} flexDirection={cart.length > 0 ? "row" : "column"} className="cart-btns">
           {
           cart.length > 0 ? <Button color="#fff" bg='#56008f' _hover={{ bg: '#6f319b' }} size='lg' onClick={removeList} mr="10px">Clear Cart</Button> : <Text mb="10px">Your shopping cart is empty.</Text>
           }
