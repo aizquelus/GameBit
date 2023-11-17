5a8beb61a9586d4ca44f83278a39017efcc7b0fa
Aizquel Urdaneta <aizquelus@gmail.com>
2023-11-17 13:13:31 -0300

    Ajustando estilos

--- src/components/Brief.jsx
+++ src/components/Brief.jsx
@@ -37,7 +37,7 @@ const Brief = () => {
                                         </Text>
 
                                         <NumberInput
-                                            size='sm'
+                                            size='lg'
                                             maxW={20}
                                             defaultValue={itemQty}
                                             min={1}
